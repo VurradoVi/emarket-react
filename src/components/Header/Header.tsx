dd1ef0ff203a7a64d7c114088587b2e562cccd85
Vlad <02kvg03@mail.ru>
2024-12-05 19:55:53 +0300

    first commit

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,27 @@
+import styles from './Header.module.scss'
+
+export default function Header() {
+  return (
+    <div className={styles.header}>
+      <h1>Emarket</h1>
+      <div className={styles.searchInput}>
+        <img src="../search.svg" alt="search" />
+      <input type="text" placeholder='Поиск'  />
+      </div>
+      <ul>
+        <li>
+            <img src="../catalog.svg" alt="Каталог" />
+            <p>Каталог</p>
+        </li>
+        <li>
+            <img src="../basket.svg" alt="Корзина" />
+            <p>Корзина</p>
+        </li>
+        <li>
+            <img src="../profile.svg" alt="Профиль" />
+            <p>Профиль</p>
+        </li>
+      </ul>
+    </div>
+  )
+}
